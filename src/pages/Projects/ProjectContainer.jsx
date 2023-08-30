c275c955a849e9b4c0b8a0ef45ea96d1fb5d8996
lsmith <therealshaunsred@gmail.com>
2023-08-30 19:46:32 -0400

    Added alt text to images

--- src/pages/Projects/ProjectContainer.jsx
+++ src/pages/Projects/ProjectContainer.jsx
@@ -2,7 +2,7 @@ export default function ProjectContainer({ project }) {
   return (
     <div className="project-container">
       <h1>{project.name}</h1>
-      <img src={project.image} />
+      <img src={project.image} alt={project.name}/>
       <div className="buttons">
         <a href={project.git}><button>Github</button></a>
         <a href={project.live}><button>Live</button></a>
