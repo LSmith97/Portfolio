41f108abd9b8e0f30b5143e7da568f22eb1efcfc
lsmith <therealshaunsred@gmail.com>
2023-10-05 14:38:09 -0400

    Updated projects page

--- src/pages/Projects/ProjectContainer.jsx
+++ src/pages/Projects/ProjectContainer.jsx
@@ -2,17 +2,27 @@ export default function ProjectContainer({ project }) {
   return (
     <div className="project-container">
       <h1>{project.name}</h1>
+      <p>{project.disc}</p>
+      <h4>{project.skills}</h4>
+      <hr />
       <img src={project.image} alt={project.name} />
+      <hr />
       <div className="buttons">
         <a href={project.git}>
-          <button>Github</button>
+          <img
+            src={"https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-social-github-1024.png"}
+            alt="github"
+          />
+          GitHub
         </a>
         <a href={project.live}>
-          <button>Live</button>
+          <img
+            src="https://i.imgur.com/9WKKj19.png"
+            alt="live"
+          />
+          Live Site
         </a>
       </div>
-
-      <p>{project.disc}</p>
     </div>
   );
 }
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -6,24 +6,15 @@ function Projects(props) {
   // create state to hold projects
   const [projects, setProjects] = useState(null);
 
-  //create function to make api call
+  // fetch data from projects.json
   const getProjectsData = async () => {
-
-		//make api call and get response
     const response = await fetch("./projects.json");
-
-		// turn response into javascript object
     const data = await response.json();
-
-		// set the projects state to the data
     setProjects(data);
-
   };
 
-  // make an initial call for the data inside a useEffect, so it only happens once on component load
   useEffect(() => {getProjectsData() }, []);
 
-  // define a function that will return the JSX needed once we get the data
   const loaded = () => {
     return projects.map((project) => (<ProjectContainer project={project}/>));
   };
